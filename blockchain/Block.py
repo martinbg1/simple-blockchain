dd6b4679873bc8805ced49421fc6cb321f1f2f94
martinbg1 <martin.b.gjerde@gmail.com>
2018-01-16 17:02:21 +0100

    Add Block.py

---
+++ blockchain/Block.py
@@ -0,0 +1,21 @@
+import datetime
+import hashlib
+
+class Block:
+    def __init__(self, data, timestamp, prevHash):
+        self.prevHash = prevHash
+        self.data = data
+        self.timestamp = timestamp
+        self.contents = [data, timestamp, prevHash]
+
+        self.blockHash = sha256_hash(contents)
+
+        @staticmethod
+        def sha256_hash(self, c):
+            c = str(c)
+            inner_hash = hashlib.sha256(c.encode()).hexdigest().encode()
+            outer_hash = hashlib.sha256(inner_hash).hexdigest()
+            return outer_hash
+
+    def genesis_block():
+        return Block ("The very first block", datetime.datetime.now(), "0")
