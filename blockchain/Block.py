ca037ec91e872cf52e5863a038d446883f492a28
martinbg1 <martin.b.gjerde@gmail.com>
2018-01-16 18:01:41 +0100

    Fix self. problem

--- blockchain/Block.py
+++ blockchain/Block.py
@@ -1,21 +1,17 @@
-import datetime
 import hashlib
 
 class Block:
     def __init__(self, data, timestamp, prevHash):
-        self.prevHash = prevHash
         self.data = data
         self.timestamp = timestamp
+        self.prevHash = prevHash
         self.contents = [data, timestamp, prevHash]
 
-        self.blockHash = sha256_hash(contents)
+        self.blockHash = self.sha256_hash(self.contents)
 
     def sha256_hash(self, c):
         c = str(c)
-        inner_hash = hashlib.sha256(c.encode()).hexdigest().encode()
-        outer_hash = hashlib.sha256(inner_hash).hexdigest()
+        #double hashing
+        inner_hash = hashlib.sha256(c.encode()).hexdigest()
+        outer_hash = hashlib.sha256(inner_hash.encode()).hexdigest()
         return outer_hash
-
-    @staticmethod
-    def genesis_block():
-        return Block ("The very first block", datetime.datetime.now(), "0")
