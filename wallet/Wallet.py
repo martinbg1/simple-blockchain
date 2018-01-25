79a7cf15bc91f400ca962d98607ea8dbc268c611
Martin Bondkall Gjerde <martin.b.gjerde@gmail.com>
2018-01-25 10:34:09 +0100

    change folder and file name to wallet

---
+++ wallet/Wallet.py
@@ -0,0 +1,25 @@
+
+
+class Wallet:
+    def __init__(balance):
+        self.balance = balance
+
+        self.publicKey = generate_publicKey()
+        self.privateKey = generate_privateKey()
+
+
+    def generate_publicKey():
+
+        #FILL
+
+    def generate_privateKey():
+
+        #FILL
+
+    def transaction(privateKey, publicKey):
+
+        #FILL
+
+    def check_privateKey():
+
+        #FILL
