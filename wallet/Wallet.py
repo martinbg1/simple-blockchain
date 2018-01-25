771411ec643fc357a63a9a73abae56d7189d3f36
Martin Bondkall Gjerde <martin.b.gjerde@gmail.com>
2018-01-25 19:49:55 +0100

    Fix publicKey and privateKey

--- wallet/Wallet.py
+++ wallet/Wallet.py
@@ -3,29 +3,33 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 class Wallet:
-    def __init__(balance):
+    def __init__(self,balance):
         self.balance = balance
 
-        self.key = rsa.generate_private_key(backend=crypto_default_backend(),public_exponent=65537,key_size=2048)
-        self.publicKey = generate_publicKey(key)
-        self.privateKey = generate_privateKey(key)
+        self.raw_key = rsa.generate_private_key(backend=crypto_default_backend(),public_exponent=65537,key_size=512)
+        self.publicKey = self.generate_publicKey(self.raw_key)
+        #Not safe to store privateKey here. This is meant as a demonstration
+        self.privateKey = self.generate_privateKey(self.raw_key)
 
 
-    def generate_publicKey(key):
+    def generate_publicKey(self, key):
         publicKey = key.public_key().public_bytes(crypto_serialization.Encoding.OpenSSH,crypto_serialization.PublicFormat.OpenSSH)
         return publicKey
 
-    def generate_privateKey(key):
+    def generate_privateKey(self, key):
         privateKey = key.private_bytes(crypto_serialization.Encoding.PEM,crypto_serialization.PrivateFormat.PKCS8,crypto_serialization.NoEncryption())
         return privateKey
-'''
-    def transaction(privateKey, publicKey):
+
+#    def transaction(privateKey, publicKey):
 
         #FILL
 
-    def check_privateKey():
+    def check_privateKey(self, privateKey):
+        return privateKey == self.privateKey
 
-        #FILL
-'''
-test = Walllet(100)
-print(test.publicKey)
+
+test_wallet = Wallet(100)
+test_walelt_2 = Wallet(500000)
+#Again, not safe to have access to the privateKey this way
+print(test_wallet.publicKey, "\n\n", test_wallet.privateKey, "\n\n")
+print(test_walelt_2publicKey, "\n\n", test_walelt_2.privateKey, "\n\n")
