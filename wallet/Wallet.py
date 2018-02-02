4445249f97d9ee21817ff01db64569b00cc3e529
martinbg1 <martin.b.gjerde@gmail.com>
2018-02-02 16:25:55 +0100

    Add transaction method

--- wallet/Wallet.py
+++ wallet/Wallet.py
@@ -3,7 +3,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 class Wallet:
-    def __init__(self,balance):
+    def __init__(self, balance):
         self.balance = balance
 
         self.raw_key = rsa.generate_private_key(backend=crypto_default_backend(),public_exponent=65537,key_size=512)
@@ -20,16 +20,34 @@ class Wallet:
         privateKey = key.private_bytes(crypto_serialization.Encoding.PEM,crypto_serialization.PrivateFormat.PKCS8,crypto_serialization.NoEncryption())
         return privateKey
 
-#    def transaction(privateKey, publicKey):
-
-        #FILL
+    def transaction(self, recv_publicKey, sender_privateKey, amount, node_wallets):
+        flag = False
+        if self.check_privateKey(sender_privateKey) and amount >= self.balance:
+            print("pending...")
+            for wallet in node_wallets:
+                if recv_publicKey == wallet.publicKey:
+                    self.balance -= amount
+                    wallet.balance += amount
+                    print("transaction conpleted. Sent ", amount, " to ", wallet.publicKey)
+                    flag = True
+                    break
+        if not flag:
+            print("Transaction failed...")
 
     def check_privateKey(self, privateKey):
-        return privateKey == self.privateKey
-
-
+        return self.privateKey == privateKey
+        
+#test wallets
 test_wallet = Wallet(100)
 test_walelt_2 = Wallet(500000)
+node = [test_wallet, test_walelt_2]
 #Again, not safe to have access to the privateKey this way
+#public key and private key test
 print(test_wallet.publicKey, "\n\n", test_wallet.privateKey, "\n\n")
-print(test_walelt_2publicKey, "\n\n", test_walelt_2.privateKey, "\n\n")
+print(test_walelt_2.publicKey, "\n\n", test_walelt_2.privateKey, "\n\n")
+#transaction test
+print(test_wallet.balance, "\t", test_walelt_2.balance)
+test_wallet.transaction("Failed attempt", test_wallet.privateKey, 100, node)
+print(test_wallet.balance, "\t", test_walelt_2.balance, "\n\n")
+test_wallet.transaction(test_walelt_2.publicKey, test_wallet.privateKey, 100, node)
+print(test_wallet.balance, "\t", test_walelt_2.balance)
